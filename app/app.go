67277f2164bae62ca39a6cbb636b31a88c3d2c8d
Maedchen08 <ratihsarifajar18@gmail.com>
2021-09-13 11:33:43 +0700

    add config

--- app/app.go
+++ app/app.go
@@ -1,1 +1,15 @@
 package app
+
+import ("AntarJemput-Be-C/config")
+
+type Application struct {
+	Config *config.Config
+}
+
+func Init() *Application {
+	application := &Application{
+		Config: config.Init(),
+	}
+
+	return application
+}
