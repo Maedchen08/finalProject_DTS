4a07eb095dc4cd1e6dc8e6d895e49ba245a013c1
rahmaasdarina <rahmaasdarina@gmail.com>
2021-10-14 00:02:21 +0700

    cari agen

---
+++ models/agent_search.go
@@ -0,0 +1,14 @@
+package models
+
+import "gorm.io/gorm"
+
+type AgentSearch struct {
+	gorm.Model
+	TypeTransactionId int    `gorm:"column:type_transaction_id" json:"type_transaction_id"`
+	CustomerId        int    `gorm:"column:customers_id" json:"customers_id"`
+	Address           string `gorm:"column:address;type:varchar(255);not null" json:"address"`
+	ProvinceId        int    `gorm:"column:transaction_prov_id;not null" json:"transaction_prov_id"`
+	RegencyId         int    `gorm:"column:transaction_regency_id;not null" json:"transaction_regency_id"`
+	DistrictId        int    `gorm:"column:transaction_district_id;not null" json:"transaction_district_id"`
+	Amount            int    `gorm:"column:amount;not null" json:"amount"`
+}
